3b2890e081529ff8ecbe13c86a866e99be5082c7
Ashish Shrestha <freak_asis@hotmail.com>
2023-12-29 11:35:37 +0545

    Basic Folder Added

---
+++ Basic/Area.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+void main()
+{
+    float area = 3.14 * 10 * 10;
+    printf("The area of circle is:  %f", area);
+}
---
+++ Basic/Hello.c
@@ -0,0 +1,13 @@
+# include<stdio.h>
+int main(){
+    int a,b;
+    printf("Hello World");
+    printf("Enter first number:");
+    scanf("%d", &a);
+    printf("Enter second number:");
+    scanf("%d", &b);
+    int c;
+    c = a + b;
+    printf("Sum= %d",c);
+    return 0;
+}
